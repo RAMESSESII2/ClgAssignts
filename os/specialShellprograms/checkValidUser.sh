1be4f9a7c06a43727781e5ab2e4562a5167ba29d
Satyam Bhardwaj <stmbhardwaj@gmail.com>
2021-02-15 16:23:47 +0530

    more comments

--- os/specialShellprograms/checkValidUser.sh
+++ os/specialShellprograms/checkValidUser.sh
@@ -1,5 +1,6 @@
 echo "User Name: "
 read name
+# just checking through the passwd file if $user i.e., a pattern such exists or not
 if grep -q "$name" /etc/passwd;
 then
     echo "user available"
--- os/specialShellprograms/searchForFiles.sh
+++ os/specialShellprograms/searchForFiles.sh
@@ -1,1 +1,3 @@
 grep -ir -L --include \*.c "97sd*7#23@@#@@987j" ../ > out.txt
+# -r = recursively search for pattern "97sd*7#23@@#@@987j", 
+# -L = if pattern NOT found that output all such files having extension .c in "out.txt"
